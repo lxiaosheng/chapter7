db6239c5e1ca0f649bb2ac83d894befeaf9196d4
root <root@localhost.localdomain>
2017-05-06 12:17:42 +0800

    Adding new modules

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -4,9 +4,7 @@ import os
 
 def run(**args):
     
-    print "[*] In dirlister module.目录列表："
-    files = os.listdir(".")  #当前目录
+    print("[*] In dirlister module.目录列表：")
+    files = os.listdir(".") 
     
     return str(files)
-
-#print run()
